cf5c51ed4af8e66b0b991eea7a73022b2118ea15
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-08-04 16:50:42 +0300

    add footer line

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,114 +13,120 @@ import { ReactComponent as Twitter } from "../../assets/icons/footer/socialMedia
 import bigLogo from "../../assets/img/footer/bigLogo.png";
 const Footer = () => {
 	return (
-		<div className={styles.container}>
-			<Link to='/'>
-				<img
-					src={bigLogo}
-					alt='logo'
-				/>
-			</Link>
-
-			<div>
-				<h4 className={styles.title}>Информация</h4>
-				<ul className={styles.navList}>
-					<li>
-						<NavLink
-							className={styles.navLink}
-							to='/'>
-							Главная
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							className={styles.navLink}
-							to='gallery'>
-							Галерея
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							className={styles.navLink}
-							to='projects'>
-							Проекты
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							className={styles.navLink}
-							to='certificates'>
-							Сертификаты
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							className={styles.navLink}
-							to='contacts'>
-							Контакты
-						</NavLink>
-					</li>
-				</ul>
+<div className={styles.flexCont}>
+			<div className={styles.container}>
+				<Link to='/'>
+					<img
+						src={bigLogo}
+						alt='logo'
+					/>
+				</Link>
+	
+				<div>
+					<h4 className={styles.title}>Информация</h4>
+					<ul className={styles.navList}>
+						<li>
+							<NavLink
+								className={styles.navLink}
+								to='/'>
+								Главная
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={styles.navLink}
+								to='gallery'>
+								Галерея
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={styles.navLink}
+								to='projects'>
+								Проекты
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={styles.navLink}
+								to='certificates'>
+								Сертификаты
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={styles.navLink}
+								to='contacts'>
+								Контакты
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<h4 className={styles.title}>Контакты</h4>
+					<ul className={styles.contactList}>
+						<li>
+							<Link
+								className={styles.contactLink}
+								to='https://goo.gl/maps/Z5adjri6u8g9tkpN6'
+								target='_blank'
+								rel='noopener noreferrer nofollow'>
+								<Icon3 />
+								<span>
+									100000, Республика Казахстан, г. Караганда, ул. Телевизионная 10
+								</span>
+							</Link>
+						</li>
+						<li>
+							<Link
+								className={styles.contactLink}
+								to='tel:+380000000000'>
+								<Icon2 />
+								<span>+38 (000) 000 00 00</span>
+							</Link>
+						</li>
+						<li>
+							<Link
+								className={styles.contactLink}
+								to='mailto:mail@gmail.com'
+								lang='en'>
+								<Icon1 />
+								<span>mail@gmail.com</span>
+							</Link>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<h4 className={styles.title}>Социальные сети</h4>
+					<ul className={styles.socialIconsList}>
+						<li>
+							<Link>
+								<Facebook />
+							</Link>
+						</li>
+						<li>
+							<Link>
+								<Linked />
+							</Link>
+						</li>
+						<li>
+							<Link>
+								<Pininterest />
+							</Link>
+						</li>
+						<li>
+							<Link>
+								<Twitter />
+							</Link>
+						</li>
+					</ul>
+				</div>
+				
 			</div>
-			<div>
-				<h4 className={styles.title}>Контакты</h4>
-				<ul className={styles.contactList}>
-					<li>
-						<Link
-							className={styles.contactLink}
-							to='https://goo.gl/maps/Z5adjri6u8g9tkpN6'
-							target='_blank'
-							rel='noopener noreferrer nofollow'>
-							<Icon3 />
-							<span>
-								100000, Республика Казахстан, г. Караганда, ул. Телевизионная 10
-							</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							className={styles.contactLink}
-							to='tel:+380000000000'>
-							<Icon2 />
-							<span>+38 (000) 000 00 00</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							className={styles.contactLink}
-							to='mailto:mail@gmail.com'
-							lang='en'>
-							<Icon1 />
-							<span>mail@gmail.com</span>
-						</Link>
-					</li>
-				</ul>
-			</div>
-			<div>
-				<h4 className={styles.title}>Социальные сети</h4>
-				<ul className={styles.socialIconsList}>
-					<li>
-						<Link>
-							<Facebook />
-						</Link>
-					</li>
-					<li>
-						<Link>
-							<Linked />
-						</Link>
-					</li>
-					<li>
-						<Link>
-							<Pininterest />
-						</Link>
-					</li>
-					<li>
-						<Link>
-							<Twitter />
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</div>
+			<span className={styles.allRights}>
+					© 2019 Digital Project. Все права защищены.
+				</span>
+</div>
 	);
 };
 
