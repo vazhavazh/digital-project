9b97f1d550efe0ec2d8a40878f0f481f136573b3
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-08-04 10:09:07 +0300

    connect with us

--- src/components/ConnectWithUs/ConnectWithUs.jsx
+++ src/components/ConnectWithUs/ConnectWithUs.jsx
@@ -1,80 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ConnectWithUs.module.scss";
+import { IoMdCheckmark } from "react-icons/io";
+import { BsArrowRight } from "react-icons/bs";
+import photoConnectWithUs from "../../assets/img/contactWithUs/img1.png"
 
 const ConnectWithUs = () => {
+	const [checked, setChecked] = useState(false);
+
+	const handleCheckboxChange = () => {
+		setChecked(!checked);
+	};
 	return (
 		<div className={styles.container}>
 			<h3 className={styles.title}>Связаться с нами</h3>
-			<form className={styles.form}>
-				<label className={styles.placeinput}>
-					<input
-						required
-						type='text'
-						id='text'
-					/>
-					<div className={styles.place_holder}>Имя</div>
-				</label>
 
-				<label className={styles.placeinput}>
-					<input
-						required
-						type='text'
-						id='text'
-					/>
-					<div className={styles.place_holder}>
-						Номер телефона<span>*</span>
-					</div>
-				</label>
-				<label className={styles.placeinput}>
-					<input
-						required
-						type='text'
-						id='text'
-					/>
-					<div className={styles.place_holder}>
-						E-mail<span>*</span>
-					</div>
-				</label>
-				<label className={styles.placeinput}>
-					<input
-						required
-						type='text'
-						id='text'
-					/>
-					<div className={styles.place_holder}>Интересующий товар/услуга</div>
-				</label>
-				<label className={styles.placeinput}>
-					<textarea
-						required
-						type='text'
-						id='text'
-					/>
-					<div className={styles.place_holder}>
-						Сообщение<span>*</span>
-					</div>
-				</label>
+			<div className={styles.flexContainer}>
+				<form className={styles.form}>
+					<label className={styles.placeinput}>
+						<input
+							type='text'
+							id='name'
+							required
+						/>
+						<div className={styles.place_holder}>Имя</div>
+					</label>
 
-				{/* <input
-					type='text'
-					placeholder='Имя'
-				/>
-				<input
-					type='text'
-					placeholder='Номер телефона '
-				/>
-				<input
-					type='text'
-					placeholder='E-mail'
-				/>
-				<input
-					type='text'
-					placeholder='Интересующий товар/услуга'
-				/>
-				<textarea
-					type='text'
-					placeholder='Сообщение'
-				/> */}
-			</form>
+					<label className={styles.placeinput}>
+						<input
+							id='tel'
+							required
+							type='tel'
+						/>
+						<div className={styles.place_holder}>
+							Номер телефона<span>*</span>
+						</div>
+					</label>
+					<label className={styles.placeinput}>
+						<input
+							required
+							type='email'
+							id='email'
+						/>
+						<div className={styles.place_holder}>
+							E-mail<span>*</span>
+						</div>
+					</label>
+					<label className={styles.placeinput}>
+						<input
+							id='good'
+							type='text'
+							required
+						/>
+						<div className={styles.place_holder}>Интересующий товар/услуга</div>
+					</label>
+					<div className={styles.textareaContainer}>
+						<label className={styles.placeinput}>
+							<textarea
+								required
+								type='text'
+								id='message'
+							/>
+
+							<div className={styles.place_holderTextArea}>
+								Сообщение<span>*</span>
+							</div>
+						</label>
+					</div>
+
+					<label className={styles.checkboxLabel}>
+						<input
+							className={styles.checkbox}
+							type='checkbox'
+							checked={checked}
+							onChange={handleCheckboxChange}
+						/>
+						<span className={styles.iconWrapper}>
+							{checked && <IoMdCheckmark />}
+						</span>
+						<span className={styles.rule}>
+							Отправляя заявку Вы соглашаетесь с политикой конфиденциальности
+						</span>
+					</label>
+					<button className={styles.ourProjectBtn}>
+						ОТПРАВИТЬ
+						<span className={styles.iconWrapperNew}>
+							<BsArrowRight className={styles.icon} />
+						</span>
+					</button>
+				</form>
+				<div className={styles.photoWrapper}>
+					<img
+						src={photoConnectWithUs}
+						alt='Person call via mobile'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
